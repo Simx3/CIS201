7d28978e9921cb77828d204039104f0b09473710
Simx3 <127240668+Simx3@users.noreply.github.com>
2023-05-13 12:45:51 +0300

    Delete Project directory

--- Project/src/js/Mscript.js
+++
@@ -1,6 +0,0 @@
-let image = document.getElementById('image');
-let images = ['new1.jpeg','new4.jpeg','new5.jpeg']
-setInterval(function(){
-  let random = Math.floor(Math.random() * 3);
-  image.src = images[random];
-}, 2200);
